2b6c7fb193d450661647c1f48bf2a7174f2df0ed
Dennis Wu <wud.bu.edu>
2018-03-23 14:51:06 -0400

    twitter hometimeline fix
    it displays username and tweet

--- CS411_teamassignment3/app.py
+++ CS411_teamassignment3/app.py
@@ -20,10 +20,9 @@ api = twitter.Api(consumer_key=consumer_key,
 credentials = api.VerifyCredentials()
 
 a = api.GetHomeTimeline(contributor_details = True)
-home_timeline = [i.text for i in a]
+home_timeline = [[i.text, i.user.screen_name] for i in a]
 #home_timeline_users = [i.user for i in a]
 
-print(home_timeline)
 
 b = api.GetUserTimeline(screen_name='nhuang54')
 user_timeline = [i.text for i in b]
@@ -57,11 +56,11 @@ def twitter():
 
 @app.route('/twitter/hometimeline')
 def hometimeline():
-    return render_template('twitter_sections.html', tweets= home_timeline)
+    return render_template('hometimeline.html', tweets= home_timeline)
 
 @app.route('/twitter/usertimeline')
 def usertimeline():
-    return render_template('twitter_sections.html', tweets = user_timeline)
+    return render_template('usertimeline.html', tweets = user_timeline)
 
 
 if __name__ == '__main__':
